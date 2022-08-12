8d57f9b7cb736bf6b37801d09e9d1329b23c8bf4
mdlufy <german.panov.2018@mail.ru>
2022-08-12 15:14:00 +0300

    get data using controlled input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React, { useState } from 'react';
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
+import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import "./styles/App.css";
 
 function App() {
-    const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
+    const [posts, setPosts] = useState([
+        { id: 1, title: "JavaScript", body: "Description" },
+        { id: 2, title: "JavaScript 2", body: "Description" },
+        { id: 3, title: "JavaScript 3", body: "Description" },
+    ]);
+    const [title, setTitle] = useState('');
 
-  return (
-    <div className="App">
-        <ClassCounter/>
-    </div>
-  );
+    function addNewPost(e) {
+        e.preventDefault();
+        console.log(title);
+    }
+
+    return (
+        <div className="App">
+            <form>
+                {/*Управляемый компонент*/}
+                <MyInput
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type="text" 
+                    placeholder="Название поста" 
+                />
+                <MyInput type="text" placeholder="Описание поста" />
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title="Посты про JS" />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Counter = function () {
     const [count, setCount] = useState(0);
 
     function increment() {
-        setCount(count + 1)
+        setCount(count + 1);
     }
-    
+
     function decrement() {
-        setCount(count - 1)
+        setCount(count - 1);
     }
 
     return (
@@ -16,9 +16,8 @@ const Counter = function () {
             <h1>{count}</h1>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
-
         </div>
-    )
-}
+    );
+};
 
 export default Counter;
