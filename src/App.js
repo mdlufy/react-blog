83ea4d6022f5dd4c3651b7b2f38aee1739784ebf
mdlufy <german.panov.2018@mail.ru>
2022-08-13 16:59:44 +0300

    change post list animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
