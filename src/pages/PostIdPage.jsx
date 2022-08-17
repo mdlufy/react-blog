5893eaa443a5a29950f4a914df8b642c13f092ec
mdlufy <german.panov.2018@mail.ru>
2022-08-17 20:18:23 +0300

    add comments route to each post

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,17 +7,23 @@ import { useFetching } from '../hooks/useFetching';
 const PostIdPage = () => {
     const params = useParams();
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
     const [fetchPostById, isLoading, error] = useFetching(async (id) => {
         const response = await PostService.getById(id);
         setPost(response.data);
     });
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(id);
+        setComments(response.data);
+    });
 
     useEffect(() => {
         fetchPostById(params.id);
+        fetchComments(params.id);
     }, [])
 
     return (
-        <div style={{textAlign: 'center'}}>
+        <div>
            <h1>Вы открыли страницу поста с ID = {params.id}</h1> 
            {isLoading 
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader /></div>
@@ -26,6 +32,17 @@ const PostIdPage = () => {
            <h1>
                Комментарии
            </h1>
+           {isComLoading 
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader /></div>
+                : <div>
+                    {comments.map(comm => 
+                        <div style={{marginTop: 15}}>
+                            <h5>{comm.email}</h5>
+                            <div>{comm.body}</div>
+                        </div>
+                    )}
+                </div>
+           }
 
         </div>
     );
