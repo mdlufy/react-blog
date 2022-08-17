899b939a123708103b432cdefb275ecb60cddba9
mdlufy <german.panov.2018@mail.ru>
2022-08-17 21:07:44 +0300

    add public and private routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { routes } from "../router";
+import { publicRoutes, privateRoutes } from "../router";
 import "../styles/App.css";
 import Layout from "./Layout";
 
 const AppRouter = () => {
+    const isAuth = false;
     return (
-        <Routes>
-            <Route path="react-fundamental" element={<Layout />}>
-                {routes.map(route => 
-                    <Route element={route.element} path={route.path} />
-                )}
-                <Route path="*" element={<Navigate to="error" replace />} />
-            </Route>
-        </Routes>
+        isAuth
+            ?
+            <Routes>
+                <Route path="react-fundamental" element={<Layout />}>
+                    {privateRoutes.map(route => 
+                        <Route element={route.element} path={route.path} />
+                    )}
+                    <Route path="*" element={<Navigate to="posts" replace />} />
+                </Route>
+            </Routes>
+            :
+            <Routes>
+                <Route path="react-fundamental" element={<Layout />}>
+                    {publicRoutes.map(route => 
+                        <Route element={route.element} path={route.path} />
+                    )}
+                    <Route path="*" element={<Navigate to="login" replace />} />
+                </Route>
+            </Routes>
+        
     );
 };
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import About from "../pages/About";
 import Error from "../pages/Error";
+import Login from "../pages/Login";
 import PostIdPage from "../pages/PostIdPage";
 import Posts from "../pages/Posts";
 
 
-export const routes = [
+export const privateRoutes = [
     {path: 'about', element: <About/>},
     {path: 'posts', element: <Posts/>},
     {path: 'posts/:id', element: <PostIdPage/>},
     {path: 'error', element: <Error/>}
+]
+
+export const publicRoutes = [
+    {path: 'login', element: <Login/>},
 ]
